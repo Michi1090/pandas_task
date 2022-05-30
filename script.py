c95759aa5572ff4f9140a5b4f4958a966195a738
Michi1090 <michi.m1090@gmail.com>
2022-05-30 18:02:18 +0900

    DBよりIPアドレスを1つずつ抽出

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, false
 import subprocess
 
 # データベース接続
@@ -8,9 +8,11 @@ engine = create_engine('mysql://root:password@localhost:3306/pandas_task')
 # エクセルからデータ抽出
 df = pd.read_excel('data.xlsx')
 
-# DBに登録
+# DBに登録〜IPアドレス抽出
 with engine.begin() as con:
-    df.to_sql('pings', con=con, if_exists='replace', index=False)
+    df.to_sql('pings', con=con, if_exists='replace')
     ip = pd.read_sql('SELECT ip_address FROM pings', con=con)
 
 print(ip)
+for index, item in ip.iterrows():
+    print(item['ip_address'])
