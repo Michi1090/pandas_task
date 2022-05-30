b1a193dadd6559a60b311147c1b4390439ef3240
Michi1090 <michi.m1090@gmail.com>
2022-05-30 17:38:37 +0900

    DB登録〜IPアドレス抽出

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import subprocess
 
 # データベース接続
 engine = create_engine('mysql://root:password@localhost:3306/pandas_task')
@@ -10,6 +11,6 @@ df = pd.read_excel('data.xlsx')
 # DBに登録
 with engine.begin() as con:
     df.to_sql('pings', con=con, if_exists='replace', index=False)
+    ip = pd.read_sql('SELECT ip_address FROM pings', con=con)
 
-# データベース切断
-con.close()
+print(ip)
