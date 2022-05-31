3c8ab498b960a81075014b2c613accfd12d6c797
Michi1090 <michi.m1090@gmail.com>
2022-05-31 10:56:29 +0900

    ping実行

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from sqlalchemy import create_engine, false
-import subprocess
+from sqlalchemy import create_engine, true
+import subprocess as sub
 
 # データベース接続
 engine = create_engine('mysql://root:password@localhost:3306/pandas_task')
@@ -13,6 +13,16 @@ with engine.begin() as con:
     df.to_sql('pings', con=con, if_exists='replace')
     ip = pd.read_sql('SELECT ip_address FROM pings', con=con)
 
-print(ip)
-for index, item in ip.iterrows():
-    print(item['ip_address'])
+    # DBから個別のIPアドレス抽出〜結果をDBへ格納
+    for index, item in ip.iterrows():
+        # 各IPアドレスごとにpingコマンドを実行
+        res = sub.run(['ping', item['ip_address'], '-c', '3', '-W', '1000'],
+                    capture_output=True)
+        print(res.stdout.decode('UTF-8'))
+
+        # pingコマンド成功時は0、失敗時はそれ以外を返す
+        if res.returncode == 0:
+            print('成功\n\n')
+        else:
+            print('失敗\n\n')
+        print("--------------------------------------------------------------")
--- table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE pings(
-    id INTEGER NOT NULL PRIMARY KEY,
-    name VARCHAR(30),
-    url VARCHAR(100),
-    ip_address VARCHAR(30),
-    result BOOLEAN
-);
-
