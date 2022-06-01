43115438e88acfe63bd15c24fe564dfaebbf591a
Michi1090 <michi.m1090@gmail.com>
2022-06-01 18:15:38 +0900

    Excelから一旦DB登録する処理に変更

--- script.py
+++ script.py
@@ -1,32 +1,74 @@
+from email import charset
 import mysql.connector
 import pandas as pd
 import subprocess as sub
 import datetime
+import logging
 
 # DB接続
 conn = mysql.connector.connect(
     host='localhost',
     user='root',
     password='password',
-    database='pandas_task'
+    database='pandas_task',
+    charset = 'utf8'
 )
 cursor = conn.cursor()
 
-# エクセルからデータ抽出
+# トランザクションスタート
+# ===========================================================
+
+# 実行日時を生成
+dt = datetime.datetime.today()
+table_name = dt.strftime('%Y_%m_%d %X')
+
+# エクセルからマスタ取得
 df = pd.read_excel('data.xlsx')
 
-# ping実行〜DB登録
+# ===========================================================
+
+# テーブルの作成
+cursor.execute(f'''
+                CREATE TABLE `{table_name}` (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(30),
+                    url VARCHAR(100),
+                    ip_address VARCHAR(30),
+                    result BOOLEAN
+                );
+              ''')
+
+# 監視対象をDBに登録
 for i in range(len(df)):
     # 各データを変数に代入
     name = df.loc[i, 'name']
     url = df.loc[i, 'url']
     ip = df.loc[i, 'ip_address']
-    today = datetime.datetime.today()
-    dt = today.strftime('%Y-%m-%d %X')
+
+    # SQL発行
+    cursor.execute(f'''
+                    INSERT INTO `{table_name}`
+                    (name, url, ip_address)
+                    VALUES ('{name}', '{url}', '{ip}')
+                  ''')
+
+# DBへコミット
+conn.commit()
+
+# ==================================================================
+
+# ping実行〜結果をDBに登録
+# IPアドレスをDBより抽出
+cursor.execute(f'SELECT ip_address FROM {table_name}')
+
+for ping_ip in cursor:
 
     # pingコマンド実行
-    res = sub.run(['ping', ip, '-c', '3', '-W', '1000'],
-                  capture_output=True)
+    res = sub.run(
+                    ['ping', ping_ip, '-c', '3', '-W', '1000'],
+                    capture_output=True,
+                    shell=True
+                )
     print(res.stdout.decode('UTF-8'))
 
     # 結果の表示、及び格納
@@ -38,15 +80,30 @@ for i in range(len(df)):
         result = False
     print("--------------------------------------------------------------")
 
-    # DB登録
+    # SQL発行
     cursor.execute(f'''
-                   INSERT INTO pings
-                   (name, url, ip_address, datetime, result)
-                   VALUES ("{name}", "{url}", "{ip}", "{dt}", {result})
-                   '''
-                   )
-    conn.commit()
+                   UPDATE FROM {table_name}
+                   SET result = {result}
+                   WHERE ip_address = '{ping_ip}'
+                   ''')
+
+
+# DB登録
+conn.commit()
+
+# =================================================================
+
+# トランザクション終了
 
 # DB切断
 cursor.close()
 conn.close()
+
+# # ログ出力の設定
+# logging.basicConfig(
+#     filename='test.log',
+#     level=logging.DEBUG,
+#     format='[%(asctime)s](%(levelname)s) filename=%(filename)s(%(lineno)s): %(message)s',
+#     datefmt='%Y/%m/%d %H:%M:%S',
+# )
+# logging.error('ログを出力しました。')
